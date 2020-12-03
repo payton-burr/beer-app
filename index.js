4b49c48a2b3909ba8239c70503b2e1553103b6d3
Payton <greatpayton@gmail.com>
2020-12-03 16:49:36 -0700

    looped through api to grab info needed to render

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const urlBase = 'https://api.punkapi.com/v2/beers';
 
 async function getBeers() {
   const beerAPI = await fetch(urlBase);
-  const beer = await beerAPI.json();
+  const beers = await beerAPI.json();
   const beerDiv = document.querySelector('.beers');
+  let beerHTML = '';
 
-  for (item of beer) {
-    const div = document.createElement('div');
-    div.innerHTML = item.name;
-    beerDiv.appendChild(div);
-  }
+  beers.forEach((beer) => {
+    beerHTML += `
+    <div class="beer-wrapper card">
+        <div class="beer">
+            <img class="beer__img" src="${beer.image_url}">
+            <h3>${beer.name}</h3>
+            <span class="beer__info">
+                <span>ABV: ${beer.abv}%</span>
+                <span>IBU: ${beer.ibu}</span>
+            </span>
+        </div>
+    </div>
+    `;
+
+    beerDiv.innerHTML = beerHTML;
+  });
 }
 
 getBeers();
